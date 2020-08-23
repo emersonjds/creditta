ebfd3b8d2faf8ea04b77d31c1dade1dfb674b80b
Emerson Silva <emerson_jdss@hotmail.com>
2020-08-22 23:14:36 -0300

    starter app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Emerson</Text>
+    </SafeAreaView>
+  );
+}
